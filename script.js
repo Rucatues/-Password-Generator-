306dfc9c61ed5c0ddb2bfc9e661ce919ce790c1e
Rucatues <115330764+Rucatues@users.noreply.github.com>
2022-12-16 18:12:05 -0600

    initial commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Assignment code here
+var lowerArray = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var upperArray = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+var charactersArray = [' ', '!', '#', '$', '%', '&', '(', ')', '*', '+', ',', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ',', '^', '_', '`', '{', '}', '|', '~', '"'];
+var numberArray = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
+var comboArray = [];
+
+// Get references to the #generate element
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  var welcomeMsg = confirm('Welcome to the Password Generator! Are you ready to begin?');
+  if (welcomeMsg) {
+    console.log('User confirmed they are ready to begin.')
+  } else {
+    console.log('User was not ready to begin');
+  }
+
+  var lengthPrompt = prompt('Please choose a length of at least 8 characters and no more than 128 characters', 'Type your number here');
+  if ((lengthPrompt >= 8) && (lengthPrompt <= 128)) {
+    console.log(`User choose password with length: ${lengthPrompt}`)
+  } else {
+    console.log('User did not chose password with correct length');
+    prompt('You get one more try- choose a length of at least 8 characters and no more than 128 characters', 'Type your number here');
+  }
+
+  var lowerConfirm = confirm('Do you want lowercase letters in your password?');
+  if (lowerConfirm) {
+    comboArray = comboArray.concat(lowerArray)
+    console.log(`User confirmed they want to use lowercase letters. (${lowerConfirm}) ${comboArray}`)
+  } else {
+    console.log('User did not want to use lowercase letters.');
+  }
+
+  var upperConfirm = confirm('Do you want uppercase letters in your password?');
+  if (upperConfirm) {
+    comboArray = comboArray.concat(upperArray)
+    console.log(`User confirmed they want to use uppercase letters. (${upperConfirm}) ${comboArray}`)
+  } else {
+    console.log('User did not want to use uppercase letters.');
+  }
+
+  var charactersConfirm = confirm('Do you want characters in your password?');
+  if (charactersConfirm) {
+    comboArray = comboArray.concat(charactersArray)
+    console.log(`User confirmed they want to use characters. (${charactersConfirm}) ${comboArray}`)
+  } else {
+    console.log('User did not want to use characters.');
+  }
+
+  var numberConfirm = confirm('Do you want numbers in your password?');
+  if (numberConfirm) {
+    comboArray = comboArray.concat(numberArray)
+    console.log(`User confirmed they want to use numbers. (${numberConfirm}) ${comboArray}`)
+  } else {
+    console.log('User did not want to use numbers.');
+  }
+}
+console.log(comboArray)
+//lengthPrompt variable now stores user's numbe
